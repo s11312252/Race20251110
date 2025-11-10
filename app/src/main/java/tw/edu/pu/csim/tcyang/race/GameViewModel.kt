9c5e7414a45fccdc6a1855594a021085c8495224
S11312252 <lucyamy23780983@gmail.com>
2025-11-10 16:31:18 +0800

    v.4 拖曳

--- app/src/main/java/tw/edu/pu/csim/tcyang/race/GameViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/GameViewModel.kt
@@ -1,9 +1,13 @@
 package tw.edu.pu.csim.tcyang.race
 
+import android.R.attr.value
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GameViewModel: ViewModel() {
 
@@ -13,11 +17,37 @@ class GameViewModel: ViewModel() {
     var screenHeightPx by mutableStateOf(0f)
         private set
 
+    var gameRunning by mutableStateOf(false)
+
+    var circleX by mutableStateOf(value = 0f)
+
+    var circleY by mutableStateOf(value = 0f)
+
     // 設定螢幕寬度與高度
-    fun SetGameSize(w: Float, h: Float) {
-        screenWidthPx = w
-        screenHeightPx = h
+        fun SetGameSize(w: Float, h: Float) {
+            screenWidthPx = w
+            screenHeightPx = h
+        }
+    fun StartGame() {
+        //回到初使位置
+        circleX = 100f
+        circleY = screenHeightPx - 100f
+
+        viewModelScope.launch {
+            while (gameRunning) { // 每0.1秒循環
+                delay(100)
+                circleX += 10
+
+                if (circleX >= screenWidthPx - 100){
+                    circleX = 100f
+                }
+            }
+        }
     }
 
+    fun MoveCircle(x: Float, y: Float) {
+        circleX += x
+        circleY += y
+    }
 
 }
