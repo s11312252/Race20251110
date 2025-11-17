cf6594f5f059fcf08de781de90a90883b5e62a94
s1131225 <lucyamy23780983@gmail.com>
2025-11-17 15:02:16 +0800

    v.7 賽馬切換圖片 隨機往右跑

--- app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/GameScreen.kt
@@ -20,7 +20,15 @@ import androidx.compose.ui.unit.IntSize
 @Composable
 fun GameScreen(message: String, gameViewModel: GameViewModel) {
 
-    val imageBitmap = ImageBitmap.imageResource(R.drawable.horse0)
+    //val imageBitmap = ImageBitmap.imageResource(R.drawable.horse0)
+
+    val imageBitmaps = listOf(
+        ImageBitmap.imageResource(R.drawable.horse0),
+        ImageBitmap.imageResource(R.drawable.horse1),
+        ImageBitmap.imageResource(R.drawable.horse2),
+        ImageBitmap.imageResource(R.drawable.horse3)
+    )
+
 
 
     Box(modifier = Modifier
@@ -42,9 +50,11 @@ fun GameScreen(message: String, gameViewModel: GameViewModel) {
             )
 
             drawImage(
-                    image = imageBitmap,
-                    dstOffset = IntOffset(0, 100),
-                    dstSize = IntSize(300, 300)
+                    image = imageBitmaps[gameViewModel.horse.number],
+                dstOffset = IntOffset(
+                    gameViewModel.horse.horseX,
+                    gameViewModel.horse.horseY),
+                dstSize = IntSize(200, 200)
             )
         }
 
--- app/src/main/java/tw/edu/pu/csim/tcyang/race/GameViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/GameViewModel.kt
@@ -23,31 +23,35 @@ class GameViewModel: ViewModel() {
 
     var circleY by mutableStateOf(value = 0f)
 
-    // *** 新增：追蹤分數 ***
     var score by mutableStateOf(0)
-        private set // 僅限內部修改分數
+        private set
 
-    // 設定螢幕寬度與高度
+    val horse = Horse()
+
+
     fun SetGameSize(w: Float, h: Float) {
         screenWidthPx = w
         screenHeightPx = h
     }
     fun StartGame() {
-        //回到初使位置
         circleX = 100f
         circleY = screenHeightPx - 100f
 
         viewModelScope.launch {
-            while (gameRunning) { // 每0.1秒循環
+            while (gameRunning) {
                 delay(100)
                 circleX += 10
 
-                // *** 修改：判斷是否碰到右邊界，並增加分數 ***
-                // 圓形的右邊緣碰到螢幕右邊界 (圓心在 screenWidthPx - 圓半徑100f)
                 if (circleX >= screenWidthPx - 100f){
-                    score++ // 分數 +1
-                    circleX = 100f // 回到起點
+                    score++
+                    circleX = 100f
                 }
+
+                horse.HorseRun()
+                if (horse.horseX >= screenWidthPx - 300){
+                    horse.horseX = 0
+                }
+
             }
         }
     }
